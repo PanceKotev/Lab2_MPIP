fb768b5467df2a81838c7dc13690cb0deab8457d
Панче Котев <Panche.Kotev@students.finki.ukim.mk>
2019-12-09 00:50:32 +0100

    Initial commit

---
+++ app/src/main/java/com/laboratoriski/lab2/MoviesActivity.java
@@ -0,0 +1,46 @@
+package com.laboratoriski.lab2;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.logging.Logger;
+
+public class MoviesActivity extends AppCompatActivity {
+    Logger logger= Logger.getLogger("MoviesActivity");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.custom_menu,menu);
+        SearchView searchView=(SearchView)menu.findItem(R.id.search_item).getActionView();
+        searchView.setOnQueryTextListener(getOnQueryTextListener());
+        return true;
+    }
+    private SearchView.OnQueryTextListener getOnQueryTextListener() {
+        return new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                logger.info("Query text submitted: " + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        };
+    }
+
+}
