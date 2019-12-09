0a12447d26e742474f107452222857a7f7310700
Панче Котев <Panche.Kotev@students.finki.ukim.mk>
2019-12-09 01:38:11 +0100

    Added models

---
+++ app/src/main/java/com/laboratoriski/lab2/models/OMovie.java
@@ -0,0 +1,12 @@
+package com.laboratoriski.lab2.models;
+
+public class OMovie {
+    public String imdbID;
+    public String Title;
+    public String Year;
+    public String Poster;
+    public String Released;
+    public String Plot;
+    public Double imdbRating;
+    public Double Metascore;
+}
